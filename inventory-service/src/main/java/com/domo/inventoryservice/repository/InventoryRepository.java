b30099c109532a93b83aae3b3284f15e7aec4581
kdomo <dongho5088@naver.com>
2023-03-17 18:36:51 +0900

    [#11] feat: order service에서 webClient를 사용하여 inventory service와 통신 - order service에 webflux 의존성 추가 - order service의 placeOrder메서드에서 webClient를 활용하여 재고가 있을때만 주문이 가능하도록 로직 추가

--- inventory-service/src/main/java/com/domo/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/domo/inventoryservice/repository/InventoryRepository.java
@@ -1,11 +1,11 @@
 package com.domo.inventoryservice.repository;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.domo.inventoryservice.model.Inventory;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-	Optional<Inventory> findBySkuCode(String skuCode);
+	List<Inventory> findBySkuCodeIn(List<String> skuCodes);
 }
--- order-service/src/main/java/com/domo/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/domo/orderservice/service/OrderService.java
@@ -1,10 +1,13 @@
 package com.domo.orderservice.service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import com.domo.orderservice.dto.InventoryResponse;
 import com.domo.orderservice.dto.OrderLineItemsDto;
 import com.domo.orderservice.dto.OrderRequest;
 import com.domo.orderservice.model.Order;
@@ -17,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class OrderService {
 	private final OrderRepository orderRepository;
+	private final WebClient webClient;
 
 	public void placeOrder(OrderRequest orderRequest) {
 		List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList().stream()
@@ -28,6 +32,23 @@ public class OrderService {
 				.orderLineItemsList(orderLineItems)
 				.build();
 
+		List<String> skuCodes = order.getOrderLineItemsList().stream()
+				.map(OrderLineItems::getSkuCode)
+				.toList();
+
+		InventoryResponse[] inventoryResponseArray = webClient.get()
+				.uri("http://localhost:8082/api/inventory",
+						uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+				.retrieve()
+				.bodyToMono(InventoryResponse[].class)
+				.block();
+
+		boolean allProductIsInStock = Arrays.stream(inventoryResponseArray)
+				.allMatch(InventoryResponse::isInStock);
+
+		if (!allProductIsInStock) {
+			throw new IllegalArgumentException("Product is not in stock, please try again later");
+		}
 		orderRepository.save(order);
 	}
 
